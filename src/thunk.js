ef1c9e15118b708ccb0b611c5a44e8806b75d1ec
grafijoss <grafijoss@hotmail.com>
2020-09-06 22:56:57 -0500

    action creator

--- src/thunk.js
+++ src/thunk.js
@@ -9,30 +9,35 @@ const makeType = m => (a, isAsync) => {
 	return `${a}/${a}`
 }
 
+// makeActionCreator
+function mac(type, ...argNames) {
+	return function ac(...args) {
+		const action = { type }
+		argNames.forEach((arg, index) => {
+			action[argNames[index]] = args[index]
+		})
+		return action
+	}
+}
+
 const t = makeType('thunk')
 
 const FETCH = t('fetch', true)
 
 // it is a function that only recives the type
-const fetchStart = () => ({
-	type: FETCH.START
-})
+const fetchStart = mac(FETCH.START)
+
 // it recives a payload that it is all of data that the API return us
-const fetchSucces = payload => ({
-	type: FETCH.SUCCES,
-	payload,
-})
+const fetchSucces = mac(FETCH.SUCCES, 'payload')
+
 // it recives a error that it is all of data that the API return us
-const fetchError = error => ({
-	type: FETCH.ERROR,
-	error,
-})
+const fetchError = mac(FETCH.ERROR, 'error')
 
 const url = 'https://jsonplaceholder.typicode.com/users'
 
 const initialState = {
 	data: {
-
+		1: { name: 'Noticia' }
 	},
 	fetching: false,
 	fetched: false,
@@ -64,6 +69,7 @@ export default function reducer(state = initialState, action) {
 
 export const myThunk = payload =>
 	async (dispatch, getState) => {
+		console.log(payload)
 		dispatch(fetchStart())
 		try {
 			const result = await fetch(url) // promise
