3e5e4beb2096431358296efebb99d895f84fe6a3
grafijoss <grafijoss@hotmail.com>
2020-09-07 14:54:24 -0500

    fetching and fetched true

--- src/hors.js
+++ src/hors.js
@@ -1,5 +1,5 @@
 export const fetchReducer = ({ START, SUCCES, ERROR }) => ({
 	[START]: state => ({ ...state, fetching: true }),
-	[SUCCES]: (state, { payload }) => ({ ...state, data: payload }),
-	[ERROR]: (state, { error }) => ({ ...state, error })
+	[SUCCES]: (state, { payload }) => ({ ...state, data: payload, fetching: false, fetched: true }),
+	[ERROR]: (state, { error }) => ({ ...state, error, fetching: false })
 })
